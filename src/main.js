cd27c0d27b014dee084387679dcb4faff7cefecc
Andrea0611 <andrea.parquiroz@gmail.com>
2019-02-14 09:02:52 -0600

    cambios css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 //filtrar datos para usar los indicadores relacionados con la educació
 //const WORLDBANK = WORLDBANK;
-const dataMex = WORLDBANK.MEX.indicators;
-let filteredIndicators = [];
-const sortedData = [];
+// const WORLDBANK = window.WORLDBANK;
+const dataMex =window.WORLDBANK.MEX.indicators;
+// let filteredIndicators = [];
+let indicatorData = [];
+// const sortedData = [];
 
 const indicator = document.getElementById('indicator');
 
@@ -20,15 +22,14 @@ const print = (indicatorName, indicatorCode) => {
 
 
 //evento click en los botones
-let indicatorName = '';
-let indicatorCode = '';
-
+// let indicatorName = '';
+// let indicatorCode = '';
+let filteredIndicators = [];
 for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener('click', () => {
-    indicator.innerHTML = '';
     indicator.style.display = 'block';
-    let valElement = elements[i].value;
-    window.worldBank.filter(dataMex, valElement);
+    let valElement = elements[i].value; // event.target.value
+    filteredIndicators = window.worldBank.filter(dataMex, valElement);
     filteredIndicators.forEach(element => {
       let indicatorName = element.indicatorName;
       let indicatorCode = element.indicatorCode;
@@ -37,8 +38,11 @@ for (let i = 0; i < elements.length; i++) {
   })
 }
 
-
-
+// let indicatorYear = [];
+let indicatorValues = [];
+// let indicatorData = [];
+let completeData = [];
+let year = [];
 //función para imprimir datos de variable en el html
 const table = document.getElementById('indicator-table');
 indicator.addEventListener("change", ()=> {
@@ -50,20 +54,38 @@ indicator.addEventListener("change", ()=> {
   filteredIndicators.forEach( element => {
     if(element.indicatorCode === indicatorSelect){
       let indicatorName = element.indicatorName;
-      let year = element.data;
+      year = element.data;
       for (let data in year) {
-        indicatorYear = `<tr><td>${data}</td></tr>`;
-        indicatorData = `<tr><td>${year[data]}<td></tr>`;
+        indicatorValues = `${data}, ${year[data]}`;
+        
+        // indicatorYear = `<tr><td>${data}</td></tr>`;
+        // indicatorData = `<tr><td>${year[data]}<td></tr>`;
         document.getElementById('indicator-name').innerHTML = indicatorName + ':';
         const row = table.insertRow(0);
         const cellYear = row.insertCell(0);
         const cellData = row.insertCell(1);
-        cellYear.insertAdjacentHTML('beforeend', indicatorYear);
-        cellData.insertAdjacentHTML('beforeend', indicatorData);
+        cellYear.insertAdjacentHTML('beforeend', `<tr><td>${data}</td></tr>`);
+        cellData.insertAdjacentHTML('beforeend', `<tr><td>${year[data]}<td></tr>`);
       }
     }
+    return year
   })
 })
+
+//evento de la opción a ordenar
+const orderOption = document.getElementById('type-of-order');
+orderOption.addEventListener('change', () => {
+     window.worldBank.sort(year, orderOption.value)
+})
+// for(let i; i < orderOption.length; i++){
+//   orderOption[i].addEventListener('click', () => {
+//     if(orderOption.value === 'ascendent') {
+//       console.log(window.worldBank.sort(year, 'ascendent'))
+//     } else if(orderOption.value === 'descendent') {
+//       window.worldBank.sort(year, 'descendent')
+//     }
+//   })
+// }
 
 //botones del nav
 const whoAreWe = document.getElementById('who-are-we');
@@ -94,135 +116,17 @@ contact.addEventListener('click', () => {
   informationTwo.style.display = 'none';
 })
 
+const hamburguerButton = document.getElementById('bar');
 
-
-
-//evento de click
-// const typeIndicator = () => {
-//   button.addEventListener ('click', () => {
-//      console.log(button.value);
-//   } )
-// }
-
-
-
-//   educationButton.addEventListener('click',()=>{
-//   wordToCompare = educationButton.value;
-//   filterFunction(dataMex, wordToCompare);
-//   filteredIndicators.forEach(element => {
-//   let indicatorName = element.indicatorName;
-//   let indicatorCode = element.indicatorCode;
-//   print(indicatorName, indicatorCode);
-//   })
-
-// })
-
-
-
-
-
-
-// const education = document.getElementById('education');
-// const laboral = document.getElementById ('laboral');
-// const demographic = document.getElementById ('demographic');
-
-// // Funcion que imprime los indicadores de educación en el select
-// education.insertAdjacentHTML ("beforeend", '<option value="">Selecciona un indicador</option>');
-// const print = (indicatorName, indicatorCode) => {
-//   let result = `<option value = "${indicatorCode}" > ${indicatorName} </option>`
-//   education.insertAdjacentHTML('beforeend', result);
-// }
-
-// //función de extraer elementos:
-// educationIndicators.forEach(element => {
-//   let indicatorName = element.indicatorName;
-//   let indicatorCode = element.indicatorCode;
-//   print(indicatorName, indicatorCode)
-// });
-
-// // Funcion que imprime los indicadores de laboral en el select
-// laboral.insertAdjacentHTML ("beforeend", '<option value="">Selecciona un indicador</option>');
-// const printLaboral = (indicatorName, indicatorCode) => {
-//   let result = `<option value = "${indicatorCode}" > ${indicatorName} </option>`
-//   laboral.insertAdjacentHTML('beforeend', result);
-// }
-
-
-// //función de extraer elementos:
-// laboralIndicators.forEach(element => {
-//   let indicatorName = element.indicatorName;
-//   let indicatorCode = element.indicatorCode;
-//   printLaboral(indicatorName, indicatorCode)
-// });
-
-// // Funcion que imprime los indicadores de demográfico en el select
-
-// demographic.insertAdjacentHTML ("beforeend", '<option value="">Selecciona un indicador</option>');
-// const printDemographic = (indicatorName, indicatorCode) => {
-//   let result = `<option value = "${indicatorCode}" > ${indicatorName} </option>`
-//   demographic.insertAdjacentHTML('beforeend', result);
-// }
-
-// //función de extraer elementos:
-// demographicIndicators.forEach(element => {
-//   let indicatorName = element.indicatorName;
-//   let indicatorCode = element.indicatorCode;
-//   printDemographic(indicatorName, indicatorCode)
-// });
-
-// //función para imprimir datos de variable en el html
-// education.addEventListener("change", ()=> {
-//   document.getElementById('indicator-name').innerHTML = '';
-//   document.getElementById('indicator-result').innerHTML='';
-//   let indicatorSelect = education.value;
-//   educationIndicatiors.forEach( element => {
-//     if(element.indicatorCode == indicatorSelect){
-//       let indicatorName = element.indicatorName;
-//       let year = element.data;
-//       console.log(element.data);
-//       for (let data in year) {
-//         indicatorResult = `<ol>${data} = ${year[data]}</ol>`;
-//         document.getElementById('indicator-name').innerHTML = indicatorName + ':';
-//         document.getElementById('indicator-result').insertAdjacentHTML('beforeend', indicatorResult);
-//       }
-//     }
-//   })
-// })
-
-
-// //EVENTO EN EL INDICADOR
-// let indicatorResult = ('');
-
-// //Eventos, variables
-// const sectionOne = document.getElementById('section-1');
-// const sectionTwo = document.getElementById('section-2');
-
-// //Evento del botón "Volver a inicio"
-// // const startButton = document.getElementById('startButton');
-
-// // startButton.addEventListener('click', () => {
-// //   sectionTwo.classList.add('hide');
-// //   sectionOne.classList.remove('hide');
-// // })
-
-
-
-// /*
-// let educationData = dataMex.find(element => {
-//   element.indicatorName == /educación/i.test
-// })
-
-
-hamburguerButton = () => {
+hamburguerButton.addEventListener('click', () => {
   let x = document.getElementById("buttonHeader");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
-}
+}) 
 
 //función que ordena 
-window.worldBank.sort(filteredIndicators, 'ascendent');
-console.log(indicatorData);
 
+// console.log(filteredIndicators);
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,45 +1,23 @@
 require('../src/data.js');
 
-const dataStudent = [
-  {
-   nombre : 'Silvana',
-   edad : '32',
-   altura: '1.60',
-  },
-  {
-   nombre : 'John',
-   edad : '33',
-   altura: '1.80',
-  },
-  {
-   nombre : 'Mike',
-   edad : '25',
-   altura: '1.70',
-  },
-  {
-   nombre : 'Dení',
-   edad : '35',
-   altura: '1.50',
-  }
- ]
 
  
 //Así es como me imagino que puede ser el test...
  
  describe('filter', () => {
-  it('debería ser una función', () => {
-    expect('filter').toBe('function');
+  it('Debería ser una función', () => {
+    expect(window.filter()).toBe('function');
   });
   it ('debería retornar `[{nombre : Silvana, edad : 32, altura : 1.80}]` para: filter = (dataStudent, Silvana)', () => {
-    expect(filter(dataStudent, 'Silvana')).toBe('[{nombre : Silvana, edad : 32, altura : 1.80}]')
+    expect(window.filter(dataStudent, 'Silvana' [0])).toEqual('[{nombre : Silvana, edad : 32, altura : 1.80}]')
   })
 });
 
-sort(), () => {
+describe('sort', () => {
   it('debería ser una función', () => {
     expect('sort').toBe('function');
   })
-}
+})
 
 /*describe('example', () => {
   it('is a function', () => {
@@ -50,4 +28,3 @@ sort(), () => {
     expect(example()).toBe('example');
   });
 });*/
-
