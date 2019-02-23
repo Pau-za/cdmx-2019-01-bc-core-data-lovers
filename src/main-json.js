df7a11c7d7f300ac132e505d7e63e7d3af937144
Pau-za <molo_pau@hotmail.com>
2019-02-23 12:47:36 -0600

    tiene botón que aparece cuando haces scroll, y te lleva hasta arriba cuando das click :D. Siguiente paso, hacer funcionalidad de la página para explorar datos por indicador :O

--- src/main-json.js
+++ src/main-json.js
@@ -219,3 +219,22 @@ const meanResult = document.getElementById('mean-result');
     meanResult.innerHTML = '= ' + result.toFixed(2);
   })
 // }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+const buttonToTop = document.getElementById('button-to-top');
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    buttonToTop.style.display = "block";
+  } else {
+    buttonToTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
