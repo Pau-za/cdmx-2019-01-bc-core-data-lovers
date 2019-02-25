ed0480aded4e3c20e500aa69d9cc5c29233d3395
Pau-za <molo_pau@hotmail.com>
2019-02-25 13:23:17 -0600

    ya pone gráfica de dona del indicador seleccionado!!

--- src/main_data_page.js
+++ src/main_data_page.js
@@ -22,7 +22,7 @@ const indicatorSelect = document.getElementById('indicator-select');
 let filteredIndicators = [];
 
 //función que imprime en el select los indicadores 
-const print = (indicatorName, indicatorCode) => {
+const print = (indicatorName) => {
   const result = `<option value =  "${indicatorName}" > ${indicatorName} </option>`;
   indicatorSelect.insertAdjacentHTML('beforeend', result);
 }
@@ -38,7 +38,7 @@ for (let i = 0; i < typeOfIndicator.length; i++) {
     filteredIndicators.forEach(element => {
       let indicatorName = element.indicatorName;
       let indicatorCode = element.indicatorCode;
-      print(indicatorName, indicatorCode);
+      print(indicatorName);
     })
   })
 }
@@ -48,17 +48,26 @@ const tableSection = document.getElementById('table-section');
 const indicatorNameSelected = document.getElementById('indicator-name-selected');
 let table = document.getElementById('table');
 
-//función que imprime los indicadores en la nueva tabla
-const printIndicatorsByCountry = (yearsArr, dataCountriesArr) => {
-  // const printingYears = `<tr><td>${yearsArr}</td></tr>`;
-  // const printingPerData = `<tr><td>${dataCountriesArr[0]}</td></tr>`;
-  // const printingMexData = `<tr><td>${dataCountriesArr[1]}</td></tr>`;
-  // const printingBraData = `<tr><td>${dataCountriesArr[2]}</td></tr>`;
-  // const printingChlData = `<tr><td>${dataCountriesArr[3]}</td></tr>`;
+let perMean = [];
+let mexMean = [];
+let braMean = [];
+let chlMean = [];
+let meanOfEachCountry = [];
+let dataPerArrNumbers = [];
+let dataMexArrNumbers = [];
+let dataBraArrNumbers = [];
+let dataChlArrNumbers = [];
 let dataPerArrShorter = [];
 let dataMexArrShorter = [];
 let dataBraArrShorter = [];
 let dataChlArrShorter = [];
+let perData = [];
+let mexData = [];
+let braData = [];
+let chlData = [];
+// let sumOfMeans = [];
+//función que imprime los indicadores en la nueva tabla
+const printIndicatorsByCountry = (yearsArr, dataCountriesArr) => {
   for (let i = 0; i < yearsArr.length; i++) {
     const row = table.insertRow(0);
     const cellYear = row.insertCell(0);
@@ -66,26 +75,26 @@ let dataChlArrShorter = [];
     const cellDataMex = row.insertCell(2);
     const cellDataBra = row.insertCell(3);
     const cellDataChl = row.insertCell(4);
-    if(dataCountriesArr[0][i] !== ''){
-      let dataPerArrNumbers = Number(dataCountriesArr[0][i]);
+    if (dataCountriesArr[0][i] !== '') {
+      dataPerArrNumbers = Number(dataCountriesArr[0][i]);
       dataPerArrShorter = dataPerArrNumbers.toFixed(2);
     } else {
       dataPerArrShorter = 'N/A';
     }
-    if(dataCountriesArr[1][i] !== ''){
-      let dataMexArrNumbers = Number(dataCountriesArr[1][i]);
+    if (dataCountriesArr[1][i] !== '') {
+      dataMexArrNumbers = Number(dataCountriesArr[1][i]);
       dataMexArrShorter = dataMexArrNumbers.toFixed(2);
     } else {
       dataMexArrShorter = 'N/A';
     }
-    if(dataCountriesArr[2][i] !== ''){
-      let dataBraArrNumbers = Number(dataCountriesArr[2][i]);
+    if (dataCountriesArr[2][i] !== '') {
+      dataBraArrNumbers = Number(dataCountriesArr[2][i]);
       dataBraArrShorter = dataBraArrNumbers.toFixed(2);
     } else {
       dataBraArrShorter = 'N/A';
     }
-    if (dataCountriesArr[3][i] !== ''){
-      let dataChlArrNumbers = Number(dataCountriesArr[3][i]);
+    if (dataCountriesArr[3][i] !== '') {
+      dataChlArrNumbers = Number(dataCountriesArr[3][i]);
       dataChlArrShorter = dataChlArrNumbers.toFixed(2);
     } else {
       dataChlArrShorter = 'N/A';
@@ -95,7 +104,37 @@ let dataChlArrShorter = [];
     cellDataMex.insertAdjacentHTML('beforeend', `<tr><td>${dataMexArrShorter}</td></tr>`);
     cellDataBra.insertAdjacentHTML('beforeend', `<tr><td>${dataBraArrShorter}</td></tr>`);
     cellDataChl.insertAdjacentHTML('beforeend', `<tr><td>${dataChlArrShorter}</td></tr>`);
+    perData.push(Number(dataCountriesArr[0][i]));
+    mexData.push(Number(dataCountriesArr[1][i]));
+    braData.push(Number(dataCountriesArr[2][i]));
+    chlData.push(Number(dataCountriesArr[3][i]));
   }
+  perMean = window.worldBank.meanOfValues(perData);
+  mexMean = window.worldBank.meanOfValues(mexData);
+  braMean = window.worldBank.meanOfValues(braData);
+  chlMean = window.worldBank.meanOfValues(chlData);
+  meanOfEachCountry.push(perMean, mexMean, braMean, chlMean);
+
+}
+
+//Función de la gráfica :O
+const doughnutChart = document.getElementById('myChart');
+
+const getMyChart = (array) => {
+  let myDoughnutChart = new window.Chart(doughnutChart, {
+    type: 'doughnut',
+    data: {
+      labels: ['Perú', 'México', 'Brasil', 'Chile'],
+      datasets: [{
+        label: 'Promedio de indicador para Cada País',
+        backgroundColor: ["rgb(241, 177, 54)", "rgb(0, 92, 116)",
+          "rgb(0, 63, 65)", "rgb(213, 91, 90)"
+        ],
+        data: array,
+      }]
+    }
+  })
+  return myDoughnutChart;
 }
 
 //Agregando evento change en el select
@@ -104,6 +143,7 @@ let indicatorName = '';
 let indicatorCode = '';
 let dataCountriesArr = [];
 let yearsArr = [];
+
 indicatorSelect.addEventListener('change', () => {
   //acá va la impresión en la tabla
   table.innerHTML = '';
@@ -119,5 +159,6 @@ indicatorSelect.addEventListener('change', () => {
     dataCountriesArr.push(dataInformation);
   })
   yearsArr = Object.keys(selectedIndicator[0].data);
+  getMyChart(meanOfEachCountry, doughnutChart);
   return printIndicatorsByCountry(yearsArr, dataCountriesArr);
 })
